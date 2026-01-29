d02cc6af329a070398fc6da6436a30b788f380cc
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-01-29 22:09:52 +0530

    wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Appbar } from "@/component/Appbar";
 import { LnadingPage } from "@/component/Landingpage";
 
 
 export default function Home() {
   return (
     <div className="">
+      <Appbar/>
       <LnadingPage/>
     </div>
   );
---
+++ component/Appbar.tsx
@@ -0,0 +1,6 @@
+
+export function Appbar () {
+    return <div className="fixed top-0 p-5 flex justify-start w-full bg-linear-to-r from-purple-300 to-purple-600 ">
+          <p className=" text-black font-black text-2xl">Crypto hi Future</p>
+    </div>
+}
